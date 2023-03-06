f389714578a96638a418c9bfdcbc6ea0523ab1c9
TeaGuardian <grammarline@yandex.ru>
2023-03-06 21:54:30 +0300

    0.0.1

--- pgtg_widgets/setup.py
+++ pgtg_widgets/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='pygame_widgets',
-      version='0.0.0',
-      url='https://github.com/the-gigi/pathology',
+
+setup(name='pgtg_widgets',
+      version='0.0.1',
+      url='https://github.com/TeaGuardian/pgtg_widgets',
       license='Apache 2.0',
       description='widgets for Pygame library',
-      packages=['pygame_widgets'],
+      packages=['pgtg_widgets'],
       author_email='grammarline@yandex.ru',
       long_description=open('../README.md').read(),
       zip_safe=False)
