84078962fe3f3a119db96844a0557fdbe65e30cc
TeaGuardian <grammarline@yandex.ru>
2023-03-06 21:03:18 +0300

    0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# PygameWidgets
+# pygame_widgets
+it will help you quickly and easily create a user interface for your PyGame application
+
+>> available widgets:
+> Switch
+> Button
+> ProgressBar
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pygame_widgets',
+      version='0.0.0',
+      url='https://github.com/the-gigi/pathology',
+      license='Apache 2.0',
+      description='widgets for Pygame library',
+      packages=['pygame_widgets'],
+      author_email='grammarline@yandex.ru',
+      long_description=open('README.md').read(),
+      zip_safe=False)
